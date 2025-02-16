8e1ae3d4ad7173429903d0c6be4dcc4a00f60821
Janice <carmeljanice825@gmail.com>
2025-02-17 00:20:48 +0530

    header.js file updated

--- public/header.js
+++ public/header.js
@@ -1,5 +1,13 @@
 fetch("header.html")
-.then(response => response.text())
-.then(data => {
+  .then(response => response.text())
+  .then(data => {
     document.getElementById("header").innerHTML = data;
-});
+
+    // Wait until header is inserted before selecting elements
+    const navToggle = document.querySelector(".nav-toggle");
+    const links = document.querySelector(".links");
+
+    navToggle.addEventListener("click", function () {
+      links.classList.toggle("show-links"); // Shorter way to toggle class
+    });
+  });
